33d73fbedebbb764bf0eb9bbfcff2716a7b3a6ea
Didrik Wachtmeister <didrik.wachtmeister@gmail.com>
2013-11-22 14:57:24 +0100

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,14 +21,13 @@ describe "StaticPages" do
   describe "About page" do
     before { visit about_path }
     it { should have_selector('h1', text: 'About Us') }
-     it { should have_selector('title', text: "#{base_title} | About Us" ) }
+    it { should have_selector('title', text: "#{base_title} | About Us" ) }
   end
   
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: "#{base_title} | Contact")}
-
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title',  text: "#{base_title} | Contact")}
   end
 
 end
